f49956c739b3f46a0980bd75abf7d53ed05651a8
Wanga Mulaudzi <47391429+wangamulaudzi@users.noreply.github.com>
2024-07-12 11:05:02 +0200

    The start of the backend

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# fake-news-backend
+# Fake Detector Backend
+
+## Setup
+
+### Download the Model
+Download the pre-trained model from [this link](https://example.com/model.pkl) and place it in the `model/` directory.
---
+++ fake-detector.py
@@ -0,0 +1,9 @@
+# Back-end for the fake-detector app
+# By: Wanga Mulaudzi
+# July 2024
+
+# Import statements
+import pickle # For loading the pre-trained model
+
+# Path to pkl file
+model_path = "fake-news-backend/model/ml_model.pkl"
